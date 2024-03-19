c58a2cf179dd4948468b9892ad949b67a950c443
tusharawat01 <tushar0402tr@gmail.com>
2024-03-20 02:57:11 +0530

    FirstCommit

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // Import required modules
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
-const Redis = require('ioredis'); // Import Redis library
-const config = require('./config'); // Import the config object
+const mysql = require('mysql');
 
 // Initialize Express app
 const app = express();
-const port = config.port;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
@@ -15,10 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // MySQL Connection
 const connection = mysql.createConnection({
-  host: config.mysqlHost,
-  user: config.mysqlUser,
-  password: config.mysqlPassword,
-  database: config.mysqlDatabase
+  port: 3306,
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'data'
 });
 
 // Connect to MySQL
@@ -30,12 +29,16 @@ connection.connect(err => {
   console.log('Connected to MySQL Database');
 });
 
-// Redis Connection
-// Connect to Redis server
-const redisClient = new Redis({
-  host: config.redisHost,
-  port: config.redisPort
-}); 
+const sql = "CREATE TABLE IF NOT EXISTS submissions (username VARCHAR(50) NOT NULL,code_language VARCHAR(50) NOT NULL,stdin TEXT,source_code TEXT, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+connection.query(sql, (error, results) => {
+  if (error) {
+    console.error('Error executing SQL command:', error);
+  } else {
+    console.log('Table created successfully');
+  }
+});
+
+
 
 // API Endpoints
 
@@ -48,7 +51,7 @@ app.post('/api/submit', (req, res) => {
   connection.query(sql, [username, codeLanguage, stdin, sourceCode], (error, result) => {
     if (error) {
       console.error('Error submitting code snippet:', error);
-      res.status(500).json('Error submitting code snippet in backend:', error);
+      res.status(500).json('Error submitting code snippet:', error);
       return;
     }
     console.log('Code snippet submitted successfully');
@@ -56,33 +59,25 @@ app.post('/api/submit', (req, res) => {
   });
 });
 
+// app.get('/',(req,res) =>{
+//   res.send('Server is ready');
+// });
+
 // GET endpoint to fetch all submissions
-app.get('/api/submissions', async (req, res) => {
-  try {
-    // Check if submissions data is cached in Redis
-    const cachedData = await redisClient.get('submissions');
-    if (cachedData) {
-      console.log('Submissions data retrieved from Redis cache');
-      res.json(JSON.parse(cachedData)); // Return cached data
-    } else {
-      // If data is not cached, fetch submissions from MySQL
-      const sql = 'SELECT username, code_language, stdin, LEFT(source_code, 100) AS source_code_preview, timestamp FROM submissions';
-      connection.query(sql, (err, results) => {
-        if (err) {
-          console.error('Error fetching submissions:', err);
-          res.status(500).send('Error fetching submissions in backend');
-          return;
-        }
-        // Cache the fetched data in Redis
-        redisClient.set('submissions', JSON.stringify(results), 'EX', 3600); // Cache expiry time: 1 hour
-        console.log('Submissions data fetched from MySQL and cached in Redis');
-        res.json(results); // Return fetched data
-      });
+app.get('/api/submissions', (req, res) => {
+  // Fetch submissions from MySQL
+  const sql = 'SELECT username, code_language, stdin, LEFT(source_code, 100) AS source_code_preview, timestamp FROM submissions';
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error fetching submissions:', err);
+      res.status(500).send('Error fetching submissions in backend');
+      return;
     }
-  } catch (error) {
-    console.error('Error fetching submissions:', error);
-    res.status(500).send('Error fetching submissions in backend');
-  }
+    if(results == null){
+      req.send("empty array");
+    }
+    res.json(results);
+  });
 });
 
 // Start server
